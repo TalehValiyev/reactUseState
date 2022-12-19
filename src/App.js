69d38bae4069537546f11592fe2346bf86309f79
Taleh <taleh93@yahoo.com>
2022-12-19 20:04:32 +0400

    useState on input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-function App() {
+  const onInput = (e) => {
+    setEmail(
+      e.target.value
+        .replaceAll("com", "az")
+        .replaceAll("ru", "az")
+        .replaceAll("net", "az")
+    );
+    console.log(".az olmali");
+  };
+  const setPswrd = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 8) {
+      console.log("8 den chox olmali");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        className="first_input"
+        type="email"
+        onInput={onInput}
+        value={email}
+        placeholder=".az ile bitmeli"
+      />
+      <input
+        className="second_input"
+        type="password"
+        placeholder="8-den cox"
+        value={password}
+        onInput={setPswrd}
+      />
     </div>
   );
-}
+};
 
 export default App;
